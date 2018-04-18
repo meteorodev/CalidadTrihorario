004efc3ec45c2a9a913e7d4417c3cac2abd4584f
drosero <darwin11rv@gmail.com>
2018-04-18 16:34:44 -0500

    add index template

--- base_consulta/views.py
+++ base_consulta/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from util.loadConfig import LoadConfig
-
+from .models import BaseConf
 
 
 # Create your views here.
 def index(request):
-    lc=LoadConfig()
-    lc.getConfig()
-    return HttpResponse("Soy la funcion index.")
+    ##lc=LoadConfig()
+    #lc.getConfig()
+    listHost=BaseConf.objects.all()
+    out=[]
+    for b in listHost:
+        out.append(b.db_host+" : "+b.db_desc)
+    return HttpResponse(out)
 
 def config(request):
     return HttpResponse("Soy la funcion config")
