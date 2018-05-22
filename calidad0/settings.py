bb98659067dfd41045fa870c07a11d72a86ef7bf
drosero <darwin11rv@gmail.com>
2018-05-22 17:06:19 -0500

    add station app

--- calidad0/settings.py
+++ calidad0/settings.py
@@ -126,5 +126,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+]
 
 STATIC_URL = '/static/'
